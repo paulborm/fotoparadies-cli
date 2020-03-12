db10a20a42239a44297f1e0bd9a7361f0054abab
Paul Borm <mail@paulborm.de>
2020-03-12 21:05:18 +0100

    Remove commander from bin/fotoparadies.js

--- bin/fotoparadies.js
+++ bin/fotoparadies.js
@@ -1,21 +1,10 @@
 #!/usr/bin/env node
 
 /**
- * @fileoverview Main CLI that is run via the fotoparadies command.
+ * @fileoverview Main command that is run via the `fotoparadies` command.
  * @author Paul Borm
  */
 
 'use strict';
 
-const program = require('commander');
-const cli = require('../lib/cli');
-const pkg = require('../package.json');
-
-program
-  .version(pkg.version)
-  .requiredOption('-S, --shopNo <number>', 'add the shop number')
-  .requiredOption('-O, --orderNo <number>', 'add the order number')
-  .option('-d, --debug', 'output debugging')
-  .parse(process.argv);
-
-cli.execute(program);
+require('../lib/cli')();
